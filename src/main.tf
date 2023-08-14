82a845914d65bff7ec828f147e2b0abd862589a2
Infraestrutura <equipe.infraestrutura@ias.org.br>
2023-08-14 14:39:10 -0300

    inserção parametro user_data

--- src/main.tf
+++ src/main.tf
@@ -6,10 +6,21 @@ resource "aws_launch_template" "rhsso" {
   key_name                  = var.keyname
   vpc_security_group_ids    = var.sg_keycloak
   instance_type             = var.instance
+  user_data                 = <<-EOT
+                              #!/bin/bash
+                              yum install -y aws-cli
+                              PARAM_VALUE=$(aws ssm get-parameter --name "KC_DB_CONNSTRING" --query "Parameter.Value" --output text)
+                              PARAM_VALUE1=$(aws ssm get-parameter --name "KC_DB_USER" --query "Parameter.Value" --output text)
+                              PARAM_VALUE2=$(aws ssm get-parameter --name "KC_DB_PW" --query "Parameter.Value" --output text)
+                              echo "export KC_DB_CONNSTRING=$PARAM_VALUE" >> /etc/profile
+                              echo "export KC_DB_USER=$PARAM_VALUE1" >> /etc/profile
+                              echo "export KC_DB_PW=$PARAM_VALUE2" >> /etc/profile
+                              EOT
   tag_specifications {
     resource_type = "instance"
     tags = {
       Name = "RHSSO - Producao"
+      Project_Name = "RHSSO"
     }
   }
 }
