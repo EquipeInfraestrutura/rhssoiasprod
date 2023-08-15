b6ff45abb3b4b6910eef017e88e7b986e25cc8de
Infraestrutura <equipe.infraestrutura@ias.org.br>
2023-08-15 17:17:16 -0300

    novo nome do launch template "rhsssoprod"

--- src/main.tf
+++ src/main.tf
@@ -1,5 +1,5 @@
 # Configuração do Launch Configuration
-resource "aws_launch_template" "rhsso" {
+resource "aws_launch_template" "rhssoprod" {
   name                      = "rhsso"
   description               = "Ambiente de Producao-RHSSO"
   image_id                  = var.ami_id
