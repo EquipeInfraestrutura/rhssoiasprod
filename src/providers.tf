76a4bf5c7f80b6f12ec0f2e38684d4c60c4f49f7
Infraestrutura <equipe.infraestrutura@ias.org.br>
2023-08-15 16:52:10 -0300

    comentário

--- src/providers.tf
+++ src/providers.tf
@@ -11,15 +11,15 @@ terraform {
 
 provider "aws" {}
 
-data "template_file" "test" {
-  template = <<-EOT
-              #!/bin/bash
-              yum install aws-cli -y
-              PARAM_VALUE=$(aws ssm get-parameter --name "KC_DB_CONNSTRING" --query "Parameter.Value" --output text)
-              PARAM_VALUE1=$(aws ssm get-parameter --name "KC_DB_USER" --query "Parameter.Value" --output text)
-              PARAM_VALUE2=$(aws ssm get-parameter --name "KC_DB_PW" --query "Parameter.Value" --output text)
-              echo "export KC_DB_CONNSTRING=$PARAM_VALUE" >> /etc/profile
-              echo "export KC_DB_USER=$PARAM_VALUE1" >> /etc/profile
-              echo "export KC_DB_PW=$PARAM_VALUE2" >> /etc/profile
-              EOT
-}
+# data "template_file" "test" {
+#   template = <<-EOT
+#               #!/bin/bash
+#               yum install aws-cli -y
+#               PARAM_VALUE=$(aws ssm get-parameter --name "KC_DB_CONNSTRING" --query "Parameter.Value" --output text)
+#               PARAM_VALUE1=$(aws ssm get-parameter --name "KC_DB_USER" --query "Parameter.Value" --output text)
+#               PARAM_VALUE2=$(aws ssm get-parameter --name "KC_DB_PW" --query "Parameter.Value" --output text)
+#               echo "export KC_DB_CONNSTRING=$PARAM_VALUE" >> /etc/profile
+#               echo "export KC_DB_USER=$PARAM_VALUE1" >> /etc/profile
+#               echo "export KC_DB_PW=$PARAM_VALUE2" >> /etc/profile
+#               EOT
+# }
